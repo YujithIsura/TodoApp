93cd7d1c67dd831bedaef10dddeab4a9d17cd7a8
YujithIsura <ujithisura@gmail.com>
2022-11-21 11:18:43 +0530

    Auth0 login has been implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
-    <div>
-      Hello World..
-    </div>
+    <main className="column">
+      <h1>Auth0 Login</h1>
+      <LoginButton />
+      <LogoutButton />
+      <UserProfile />
+    </main>
   );
 }
 
